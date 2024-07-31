b4def22864cedf8c674c8f16082b4ad4d9f6d3fb
Minal Saindane <120405098+itsminal@users.noreply.github.com>
2024-07-31 20:29:22 +0530

    Create Seeding.cpp

---
+++ Seeding.cpp
@@ -0,0 +1,10 @@
+void seeding(int n) {
+	// Write your code here.
+	int i,j;
+	for(i=n;i>=1;i--){
+        for(j = i; j >=1; j--){
+            cout<<"* ";
+        }
+        cout<< "\n";
+    }
+}
