3f67df710880bded1bcfca85aef40348cbde6ea0
Minal Saindane <120405098+itsminal@users.noreply.github.com>
2024-08-03 13:26:35 +0530

    Create Increasing Number Triangle.cpp

---
+++ Increasing Number Triangle.cpp
@@ -0,0 +1,14 @@
+void nNumberTriangle(int n) {
+    // Write your code here.
+    int i,j,temp=1;
+
+    for(i=1;i<=n;i++){
+        
+        for(j=1;j<=i;j++){
+            
+            cout<<temp<<" ";
+            temp++;
+        }
+        cout<<endl;
+    }
+}
