b04a47e224b6a9c929a6cee46303177356f5e4f8
Minal Saindane <120405098+itsminal@users.noreply.github.com>
2024-08-09 14:33:35 +0530

    Create Find all factorial numbers less than or equal to n.cpp

---
+++ Find all factorial numbers less than or equal to n.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+  
+    long long fact(long long N){
+            if(N==1 || N==0){
+                return 1;
+            }
+            return N*fact(N-1);
+    }
+        
+    vector<long long> factorialNumbers(long long n) {
+        // Write Your Code here
+        vector<long long> ans;
+        long long i=1;
+        while(i<=n){
+            if(fact(i)>n){
+                break;
+            }
+            if(fact(i)<=n){
+                ans.push_back(fact(i));
+            }
+            i++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for (auto num : ans) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
